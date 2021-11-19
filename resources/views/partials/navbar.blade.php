03b492fde6ea66ee8d1afd915ed0143a9dfa189f
andreasVi <andreasvirdian07@gmail.com>
2021-11-19 09:05:46 +0700

    tampilan custom pc

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -2,7 +2,7 @@
     <header>
         <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">PCHub</a>
+                <a class="navbar-brand" href="/">PCHub</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -12,7 +12,7 @@
                             <a class="nav-link active" aria-current="page" href="#">Products</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Custom PC</a>
+                            <a class="nav-link" href="custom">Custom PC</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Support</a>
