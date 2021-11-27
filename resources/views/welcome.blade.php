62927345200949799fc24a615923f88015eddc4b
andreasVi <andreasvirdian07@gmail.com>
2021-11-27 10:52:33 +0700

    memperbaiki home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,111 +2,112 @@
 
 @section('container')
 
-<section id="slider">
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-12">
-                <div id="slider-carousel" class="carousel slide" data-ride="carousel">
-                    <ol class="carousel-indicators">
-                        <li data-target="#slider-carousel" data-slide-to="0" class="active"></li>
-                        <li data-target="#slider-carousel" data-slide-to="1"></li>
-                        <li data-target="#slider-carousel" data-slide-to="2"></li>
-                    </ol>
-                    
-                    <div class="carousel-inner">
-                        <div class="item active">
-                            <div class="col-sm-6">
-                                <h1><span>E</span>-SHOPPER</h1>
-                                <h2>Free E-Commerce Template</h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                                <button type="button" class="btn btn-default get">Get it now</button>
-                            </div>
-                            <div class="col-sm-6">
-                                <img src="img/Cooler Master MasterBox 540.png" class="girl img-responsive" alt="" />
-                                
-                            </div>
-                        </div>
-                        <div class="item">
-                            <div class="col-sm-6">
-                                <h1><span>E</span>-SHOPPER</h1>
-                                <h2>100% Responsive Design</h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                                <button type="button" class="btn btn-default get">Get it now</button>
-                            </div>
-                            <div class="col-sm-6">
-                                <img src="img/GIGABYTE GEFORCE RTX 3060 8GB.jpg" class="girl img-responsive" alt="" />
-                                
-                            </div>
-                        </div>
-                        
-                        <div class="item">
-                            <div class="col-sm-6">
-                                <h1><span>E</span>-SHOPPER</h1>
-                                <h2>Free Ecommerce Template</h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                                <button type="button" class="btn btn-default get">Get it now</button>
-                            </div>
-                            <div class="col-sm-6">
-                                <img src="img/HYPERX FURY RGB DDR4 2X8GB.jpg" class="girl img-responsive" alt="" />
-                                
-                            </div>
-                        </div>
-                        
+<!-- Start Banner Hero -->
+<div id="template-mo-zay-hero-carousel" class="carousel slide" data-bs-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="0" class="active"></li>
+        <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="1"></li>
+        <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <div class="container">
+                <div class="row p-5">
+                    <div class="mx-auto col-md-8 col-lg-6 order-lg-last">
+                        <img class="img-fluid" src="img/Cooler Master MasterBox 540.png" alt="">
                     </div>
-                    
-                    <a href="#slider-carousel" class="left control-carousel hidden-xs" data-slide="prev">
-                        <i class="fa fa-angle-left"></i>
-                    </a>
-                    <a href="#slider-carousel" class="right control-carousel hidden-xs" data-slide="next">
-                        <i class="fa fa-angle-right"></i>
-                    </a>
+                    <div class="col-lg-6 mb-0 d-flex align-items-center">
+                        <div class="text-align-left align-self-center">
+                            <div class="deskripsi">
+                                <h1 class="h1 text-success"><b>Zay</b> eCommerce</h1>
+                                <h3 class="h2">Tiny and Perfect eCommerce Template</h3>
+                                <p>
+                                    Zay Shop is an eCommerce HTML5 CSS template with latest version of Bootstrap 5 (beta 1).
+                                    This template is 100% free provided by <a rel="sponsored" class="text-success" href="https://templatemo.com" target="_blank">TemplateMo</a> website.
+                                    Image credits go to <a rel="sponsored" class="text-success" href="https://stories.freepik.com/" target="_blank">Freepik Stories</a>,
+                                    <a rel="sponsored" class="text-success" href="https://unsplash.com/" target="_blank">Unsplash</a> and
+                                    <a rel="sponsored" class="text-success" href="https://icons8.com/" target="_blank">Icons 8</a>.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="container">
+                <div class="row p-5">
+                    <div class="mx-auto col-md-8 col-lg-6 order-lg-last">
+                        <img class="img-fluid" src="img/GIGABYTE GEFORCE RTX 3060 8GB.jpg" alt="">
+                    </div>
+                    <div class="col-lg-6 mb-0 d-flex align-items-center">
+                        <div class="text-align-left">
+                            <div class="deskripsi">
+                                <h1 class="h1">Proident occaecat</h1>
+                                <h3 class="h2">Aliquip ex ea commodo consequat</h3>
+                                <p>
+                                    You are permitted to use this Zay CSS template for your commercial websites.
+                                    You are <strong>not permitted</strong> to re-distribute the template ZIP file in any kind of template collection websites.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="container">
+                <div class="row p-5">
+                    <div class="mx-auto col-md-8 col-lg-6 order-lg-last">
+                        <img class="img-fluid" src="img/HYPERX FURY RGB DDR4 2X8GB.jpg" alt="">
+                    </div>
+                    <div class="col-lg-6 mb-0 d-flex align-items-center">
+                        <div class="text-align-left">
+                            <div class="deskripsi">
+                                <h1 class="h1">Repr in voluptate</h1>
+                                <h3 class="h2">Ullamco laboris nisi ut </h3>
+                                <p>
+                                    We bring you 100% free CSS templates for your websites.
+                                    If you wish to support TemplateMo, please make a small contribution via PayPal or tell your friends about our website. Thank you.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</section><!--/slider-->
+    <a class="carousel-control-prev text-decoration-none w-auto ps-3" href="#template-mo-zay-hero-carousel" role="button" data-bs-slide="prev">
+        <i class="fas fa-chevron-left"></i>
+    </a>
+    <a class="carousel-control-next text-decoration-none w-auto pe-3" href="#template-mo-zay-hero-carousel" role="button" data-bs-slide="next">
+        <i class="fas fa-chevron-right"></i>
+    </a>
+</div>
+<!-- End Banner Hero -->
 
-<section id="slider" style="background-color:white">
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-12">
-                <div id="slider-carousel-2" class="carousel slide" data-ride="carousel">
-                    <ol class="carousel-indicators">
-                        <li data-target="#slider-carousel-2" data-slide-to="0" class="active"></li>
-                        <li data-target="#slider-carousel-2" data-slide-to="1"></li>
-                    </ol>
-                    
-                    <h1>Our Products</h1>
-                    <div class="carousel-inner">
-                        <div class="item active">
-                            <div class="col-sm-6">
-                                <img src="img/Cooler Master MasterBox 540.png" class="girl img-responsive" alt="" />
-                                <img src="img/GIGABYTE GEFORCE RTX 3060 8GB.jpg" class="girl img-responsive" alt="" />
-                                <img src="img/HYPERX FURY RGB DDR4 2X8GB.jpg" class="girl img-responsive" alt="" />
-                            </div>
-                        </div>
-                        
-                        <div class="item">
-                            <div class="col-sm-6">
-                                <img src="img/MASTERLIQUID ML240L.jpg" class="girl img-responsive" alt="" />
-                                <img src="img/RAZER BLADE 15.jpg" class="girl img-responsive" alt="" />
-                            </div>
-                        </div>
-
-                    </div>
-                    
-                    <a href="#slider-carousel-2" class="left control-carousel hidden-xs" data-slide="prev">
-                        <i class="fa fa-angle-left"></i>
-                    </a>
-                    <a href="#slider-carousel-2" class="right control-carousel hidden-xs" data-slide="next">
-                        <i class="fa fa-angle-right"></i>
-                    </a>
-                </div>
-                
-            </div>
-        </div>
+<div class="main-product">
+    <h2> Our Products</h2>
+    <div class="product1">
+        <img src="img/RAZER BLADE 15.jpg" alt="" width="80%"/>
+        <a href="#">Laptop</a>
     </div>
-</section><!--/slider-->
+    <div class="product1">
+        <img src="img/ROG Strix XG35VQ.jpg" alt="" width="80%"/>
+        <a href="#">Monitor</a>
+    </div>
+    <div class="product1">
+        <img src="img/Cooler Master MasterBox 540.png" alt="" width="80%"/>
+        <a href="#">&nbsp CPU</a>
+    </div>
+    <div class="product1">
+        <img src="img/rakit_pc.jpg" alt="" width="80%"/>
+        <a href="#">&nbsp Build PC</a>
+    </div>
+    <div class="product1">
+        <img src="img/ROG Centurion.jpg" alt="" width="80%"/>
+        <a href="#">Accessories</a>
+    </div>
+</div>
 
 @endsection
