7cf50696f7191554038bc06f3350c0035d6a96dc
andreasVi <andreasvirdian07@gmail.com>
2021-12-12 01:22:46 +0700

    update menu profile

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -2,33 +2,67 @@
 
 @section('container')
 
-<div class="header-product">
-    {{-- untuk foto profile --}}
-    <div class="img-product">
-        <img src="img/RAZER BLADE 15.jpg" alt="">
+<div class="container">
+    <div class="body-profile">
+        <div class="row">
+            <div class="image-profile col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="d-flex flex-column align-items-center text-center">
+                            <img src="https://bootdey.com/img/Content/avatar/avatar7.png" class="rounded-circle" width="150">
+                            <div class="mt-3">
+                                <h4>{{ $customer->name }}</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="data-customer col-md-6">
+                <div class="size-profile">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <h6 class="mb-0">Full Name</h6>
+                        </div>
+                        <div class="col-sm-9 text-secondary">
+                            {{ $customer->name }}
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <h6 class="mb-0">Email</h6>
+                        </div>
+                        <div class="col-sm-9 text-secondary">
+                            {{ $customer->email }}
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <h6 class="mb-0">Phone</h6>
+                        </div>
+                        <div class="col-sm-9 text-secondary">
+                            {{ $customer->phone }}
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <h6 class="mb-0">Address</h6>
+                        </div>
+                        <div class="col-sm-9 text-secondary">
+                            -
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <a class="btn btn-info " target="blank" href="#">Edit</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    {{-- data diri --}}
-    <div class="info-product">
-        <h3>Biodata Diri</h3>
-        <div class="konten">
-            <label>Nama:</label>
-            <div>{{ $customer->name }}</div>
-        </div>
-        <div class="konten">
-            <label>Email:</label>
-            <div>{{ $customer->email }}</div>
-        </div>
-        <div class="konten">
-            <label>No. Telp:</label>
-            <div>{{ $customer->phone }}</div>
-        </div>
-    </div>
-    <form method="post" action="#" class="mb-5">
-        <div id="upload-profil" class="mb-3">
-            <label for="image" class="form-label">Ganti/Upload Foto</label>
-            <input class="form-control" type="file" id="image" name="image">
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
 </div>
 @endsection
