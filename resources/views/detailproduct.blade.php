509f676921444687dcda2e48b8d08cf1965a6ec7
AntekSoeharto <cocoalongs@gmail.com>
2021-12-12 19:41:37 +0700

    Membenahi Detail Produk

--- resources/views/detailproduct.blade.php
+++ resources/views/detailproduct.blade.php
@@ -19,26 +19,20 @@
             <div class="konten-label">
                 <ul>
                     <li>Price</li>
-                    <li>Color</li>
                     <li>Stock</li>
-                    <li>Est.Weight</li>
-                    <li>Warranty</li>
                 </ul>
             </div>
             <div class="konten-label-value">
                 <ul>
                     <li>: {{ $product->price }}</li>
-                    <li>: Black</li>
                     <li>: {{ $product->stock }}</li>
-                    <li>: 4Kg</li>
-                    <li>: 1 Year Distributor Warranty</li>
                 </ul>
             </div>
         </div>
         <div class="detail-product-form">
-            <form action="/detailproduct/{{ $product->id }}" method="POST">   
+            <form action="/detailproduct/{{ $product->id }}" method="POST">
                 @csrf
-                <ul> 
+                <ul>
                     <li>Jumlah: <input type="number" class="banyak" id="quantity" name="quantity" min="1" max="1000"></li>
                     <div class="detail-product-button">
                         @if (Session::get('user') == 'customer')
@@ -52,24 +46,9 @@
         </div>
     </div>
     <div class="desc-product">
-        <label>Deskripsi</label>
+        <h4>Description</h4>
         <div>
-            <ul>
-                <li>Processor Quad-Core 10th Gen Intel® Core™ i7-1065G7 Processor with Hyper Threading 1.3 GHz / 3.9 GHz (Base/Turbo)</li>
-                <li>OS Windows® 10 Home (64-bit)</li>
-                <li>Graphics NVIDIA® GeForce® GTX 1650 4GB GDDR5 VRAM with Max-Q Design</li>
-                <li>Storage 512GB PCIe M.2</li>
-                <li>Memory 16GB LPDDR4 3733MHz dual-channel onboard memory (Fixed)</li>
-                <li>Battery 53.1Wh</li>
-                <li>Keyboard with Razer Chroma™ single-zone full key backlighting and anti-ghosting technology</li>
-                <li>Touchpad</li>
-                <li>Glass touchpad (Microsoft Precision)</li>
-                <li>Wi-Fi 6 - Intel® Wireless-AX 201 (IEEE 802.11a/b/g/n/ac), Bluetooth® 5.0 connectivity</li>
-                <li>Thunderbolt™ 3 (USB-C™), power port, 4 lanes of PCI Express</li>
-                <li>USB-C 3.1 Gen 2, power port</li>
-                <li>2x Type-A USB 3.1</li>
-                <li>Web Camera : HD 720p, Windows Hello compliant infrared camera</li>
-            </ul>
+            <h5>{{ $product->description }}</h5>
         </div>
     </div>
 </div>
